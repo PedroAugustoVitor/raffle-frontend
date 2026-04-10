d3e7acf8b45fb066113d563737ad92ecea479956
pedro <pedro.augusto@doc4doc.com.br>
2026-04-10 16:05:15 -0300

    update dist

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: true
-  },
   base: '/raffle-frontend/',
 
 })
